da7443aa3b638298fc9916f648b39a0b006bd691
Elena <dergaeva.elena@mail.ru>
2019-05-03 16:53:13 +0300

    Registration form

---
+++ src/app/shared/services/users.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs';
+
+import {User} from '../models/user.model';
+
+@Injectable()
+export class UserService {
+  constructor (private http: HttpClient) {}
+
+  getUserbyEmail(email: string): Observable<User> {
+    return this.http.get(`http://localhost:3000/users?email=${email}`)
+      .map((user: User[]) => user[0] ? user[0] : undefined);
+  }
+
+
+
+  createNewUser(user: User): Observable<User> {
+    return this.http.post('http://localhost:3000/users', user)
+  }
+}
